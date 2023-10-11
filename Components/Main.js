638032c8f3db65bec4bfcca6817b138a468b78cc
Swathihanumanthappa123 <swathiattigunda@gmail.com>
2023-10-11 16:44:28 -0700

    committed files to repo

---
+++ Components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main(){
+    return(
+        <div className = "main_body">
+        <h1 className = "heading">Fun facts about React</h1>
+        <ul className = "lists">
+            <li>Was first released in 2012</li>
+            <li>Was orginally created by Jordan Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+        </div>
+    )
+}
